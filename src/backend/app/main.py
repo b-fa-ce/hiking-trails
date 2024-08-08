3447376908dca9db2d775654f10191520492f23d
Bruno Faigle-Cedzich <faigle-cedzich@posteo.de>
2024-08-08 21:33:39 +0200

    fix/pylint workflow

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,44 +1,56 @@
-from fastapi import FastAPI, File, UploadFile, Form
 import os
-from db.database import SessionLocal
 import shutil
+
 import uvicorn
+from fastapi import FastAPI, File, Form, UploadFile
+from db.database import SessionLocal
 
 app = FastAPI()
 
 # TODO move to env
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = "uploads"
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+
 # Dependency to get DB session
 def get_db():
+    """
+    Get database session
+    """
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
+
 # Define a root `/` endpoint
-@app.get('/')
+@app.get("/")
 async def index():
-    return {'ok': True}
+    """
+    returns {'ok': True} if running
+    """
+    return {"ok": True}
+
 
 # Define a POST `/uploadGPX` endpoint
-@app.post('uploadGPX')
+@app.post("uploadGPX")
 async def upload_file(
     file: UploadFile = File(...),
     valid_until: str = Form(None),
     # TODO db: add database session here
 ) -> None:
+    """
+    POST upload gpx route
+    """
     # TODO: file verification
     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-
-    return None
+    return valid_until
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8888, reload= True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8888, reload=True)
--- src/backend/tests/test_main.py
+++ src/backend/tests/test_main.py
@@ -3,13 +3,20 @@ from starlette.testclient import TestClient
 
 from app.main import app
 
+
 @pytest.fixture(scope="module")
 def test_app():
+    """
+    app fixture
+    """
     client = TestClient(app)
     yield client
 
 
 def test_index(test_app):
+    """
+    test / route
+    """
     response = test_app.get("/")
     assert response.status_code == 200
-    assert response.json() == {'ok': True}
+    assert response.json() == {"ok": True}
